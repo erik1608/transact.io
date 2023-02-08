00d8fe0d2476e18f3e7b3b6d460225c3778277fb
Erik Ter-Davtyan <erikterdavtyan@gmail.com>
2023-02-08 19:53:57 +0400

    Indents as spaces.

--- src/main/java/com/snee/transactio/model/request/oauth2/AuthCodeRequest.java
+++ src/main/java/com/snee/transactio/model/request/oauth2/AuthCodeRequest.java
@@ -6,72 +6,72 @@ import com.snee.transactio.model.RequestModel;
 import com.snee.transactio.model.Session;
 
 public class AuthCodeRequest implements RequestModel {
-	@Expose
-	private String client_id;
+    @Expose
+    private String client_id;
 
-	@Expose
-	private String redirect_uri;
+    @Expose
+    private String redirect_uri;
 
-	@Expose
-	private String scope;
+    @Expose
+    private String scope;
 
-	@Expose
-	private String state;
+    @Expose
+    private String state;
 
-	@Expose
-	private Session sessionData;
+    @Expose
+    private Session sessionData;
 
-	public String getClientId() {
-		return client_id;
-	}
+    public String getClientId() {
+        return client_id;
+    }
 
-	public String getRedirectUri() {
-		return redirect_uri;
-	}
+    public String getRedirectUri() {
+        return redirect_uri;
+    }
 
-	public String getScope() {
-		return scope;
-	}
+    public String getScope() {
+        return scope;
+    }
 
-	public String getState() {
-		return state;
-	}
+    public String getState() {
+        return state;
+    }
 
-	public Session getSessionData() {
-		return sessionData;
-	}
+    public Session getSessionData() {
+        return sessionData;
+    }
 
-	@Override
-	public void validate() {
-		if (client_id == null || client_id.isEmpty()) {
-			throw new RequestValidationException(
-					"client_id is required"
-			);
-		}
+    @Override
+    public void validate() {
+        if (client_id == null || client_id.isEmpty()) {
+            throw new RequestValidationException(
+                    "client_id is required"
+            );
+        }
 
-		if (redirect_uri == null || redirect_uri.isEmpty()) {
-			throw new RequestValidationException(
-					"redirect_uri is required"
-			);
-		}
+        if (redirect_uri == null || redirect_uri.isEmpty()) {
+            throw new RequestValidationException(
+                    "redirect_uri is required"
+            );
+        }
 
-		if (scope == null || scope.isEmpty()) {
-			throw new RequestValidationException(
-					"scope is required"
-			);
-		}
+        if (scope == null || scope.isEmpty()) {
+            throw new RequestValidationException(
+                    "scope is required"
+            );
+        }
 
-		if (state == null || state.isEmpty()) {
-			throw new RequestValidationException(
-					"state is required"
-			);
-		}
+        if (state == null || state.isEmpty()) {
+            throw new RequestValidationException(
+                    "state is required"
+            );
+        }
 
 
-		if (sessionData == null) {
-			throw new RequestValidationException(
-					"sessionData is required"
-			);
-		}
-	}
+        if (sessionData == null) {
+            throw new RequestValidationException(
+                    "sessionData is required"
+            );
+        }
+    }
 }
